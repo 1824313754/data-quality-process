231d410c4b89b8dbb896465acfedc3ed137346ff
汪杰 <wangjie_zn@gotion.com.cn>
2025-07-02 10:00:30 +0800

    重构代码结构

--- src/main/resources/db/doris_normal_data_stats.sql
+++ src/main/resources/db/doris_normal_data_stats.sql
@@ -1,20 +1,17 @@
--- Doris 数据统计表
-CREATE TABLE `normal_data_stats` (
-  -- 维度字段
-  `vin` varchar(255) NOT NULL COMMENT "车辆VIN码",
-  `date` date NOT NULL COMMENT "数据日期", 
-  `hour` smallint NULL COMMENT "小时(0-23)",
-  `vehicleFactory` varchar(255) NULL COMMENT "车厂",
-  
-  -- 统计字段(使用聚合函数)
-  `normal_data_count` bigint SUM NULL COMMENT "正常数据条数",
-  `abnormal_data_count` bigint SUM NULL COMMENT "异常数据条数", 
-  `data_count` bigint SUM NULL COMMENT "总数据条数",
-  `time` datetime REPLACE NULL COMMENT "数据时间",
-  `last_update_time` datetime REPLACE NULL COMMENT "最近更新时间"
+CREATE TABLE `normal_data_stats`
+(
+    `vin`                 varchar(255) NOT NULL COMMENT "车辆VIN码",
+    `day_of_year`         date         NOT NULL COMMENT "数据日期",
+    `hour`                smallint NULL COMMENT "小时(0-23)",
+    `vehicleFactory`      varchar(255) NULL COMMENT "车厂",
+    `normal_data_count`   bigint SUM NULL COMMENT "正常数据条数",
+    `abnormal_data_count` bigint SUM NULL COMMENT "异常数据条数",
+    `data_count`          bigint SUM NULL COMMENT "总数据条数",
+    `time`                datetime REPLACE NULL COMMENT "数据时间",
+    `last_update_time`    datetime REPLACE NULL COMMENT "最近更新时间"
 ) ENGINE=OLAP
-AGGREGATE KEY(`vin`, `date`, `hour`, `vehicleFactory`)
-PARTITION BY RANGE(`date`)()
+AGGREGATE KEY(`vin`, `day_of_year`, `hour`, `vehicleFactory`)
+PARTITION BY RANGE(`day_of_year`)()
 DISTRIBUTED BY HASH(`vin`) BUCKETS AUTO
 PROPERTIES (
   "replication_allocation" = "tag.location.offline: 1",
